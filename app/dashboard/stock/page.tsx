700c50f03ddde3ed2dc86b9b46dfb302c34f8207
MouhammadGaouas <gaouasmouhammad@gmail.com>
2026-03-15 23:01:12 +0100

    add product card

--- app/dashboard/stock/page.tsx
+++ app/dashboard/stock/page.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import ProductCard from '@/components/ProductCard'
 
 export default function Stock() {
-  return (
-    <div>
 
+
+
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='grid grid-cols-5'>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased       `}>
 
         <Navigation />
 
